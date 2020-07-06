f5521ed0e465422133c6b0c5c804b517d006d5d9
yoojinhee03 <yoojinhee03@gmail.com>
2020-07-07 01:16:03 +0900

    사이즈&텍스트 넣기

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function showMenu(menu){
 	console.log(menu);
 	var menuContainer=document.getElementById("menu-container");
@@ -7,8 +8,52 @@ function showMenu(menu){
 
 	for(let i=0; i<menuList.length; i++){
 		// console.log(menuList[i]);
-		if(menuList[i].firstChild!=menu){
+		if(menuList[i].firstChild==menu){
+			menuContainer.children[i].style.display="block";
+		}else{
 			menuList[i].firstChild.style.color="#000000";
+			menuContainer.children[i].style.display="none";
+			console.log(i);
+			// 
 		}
 	}
 }
+
+function sizeChk(width,height){
+	console.log(width);
+	var modelContainer=document.getElementById("model-container");
+	modelContainer.style.width=width+'px';
+	modelContainer.style.height=height+'px';
+	
+}
+
+function inputTextClick(){
+	var inputText=document.createElement('div');
+	var modelContainer=document.getElementById("model-container");
+	var text=document.getElementById("text");
+	modelContainer.appendChild(inputText);
+	inputText.innerText=text.value;
+}
+
+function menuTextClick(event){
+	// x = event.clientX;
+ //    y = event.clientY; 
+ //    console.log('x좌표:' +x + ', y좌표:' + y);
+
+	// var menuText=document.getElementById('menu-text');
+	// for(let i=0; i<menuText.length; i++){
+	// 	if(menuText[i].id="inputComplete"){
+	// 		break;
+	// 	}
+	// }
+ //    var inputText=document.createElement('input');
+ //    // inputText.id='dd';
+ //    inputText.type="text";
+ //    menuText.appendChild(inputText);
+
+ //    // inputText.style.position="absolute";
+ //    inputText.style.left=x+'px';
+ //    inputText.style.top=y+'px';
+
+}
+
